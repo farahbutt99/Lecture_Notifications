4f5e5d936918eda67e85f35f65931c61ba1b7b21
farahbutt99 <mcsf19a021@pucit.edu.pk>
2021-05-25 13:26:12 +0500

    On click events created for each channel

--- Notifications/app/src/main/java/com/example/notifications_practice/MainActivity.java
+++ Notifications/app/src/main/java/com/example/notifications_practice/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.notifications_practice;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void sendOnChannel1(View view) {
+    }
+    public void sendOnChannel2(View view) {
+    }
+    public void sendOnChannel3(View view) {
     }
 }
