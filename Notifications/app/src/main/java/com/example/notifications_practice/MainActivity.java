fe91b44bb36201caf5282dbeed179bbbf8a986a5
farahbutt99 <mcsf19a021@pucit.edu.pk>
2021-05-25 13:36:38 +0500

    Variables created

--- Notifications/app/src/main/java/com/example/notifications_practice/MainActivity.java
+++ Notifications/app/src/main/java/com/example/notifications_practice/MainActivity.java
@@ -1,16 +1,27 @@
 package com.example.notifications_practice;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.NotificationManagerCompat;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+    private NotificationManagerCompat notificationManager;
+    private EditText editTextTitle;
+    private EditText editTextMessage;
 
+    public static final String CHANNEL_1_ID = "channel1";
+    public static final String CHANNEL_2_ID = "channel2";
+    public static final String CHANNEL_3_ID = "channel3";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        notificationManager = NotificationManagerCompat.from(this);
+        editTextTitle = findViewById(R.id.edit_text_title);
+        editTextMessage = findViewById(R.id.edit_text_message);
     }
 
     public void sendOnChannel1(View view) {
