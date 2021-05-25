92a9cb5a8c1f696dfbe9689904ef29d33d132110
farahbutt99 <mcsf19a021@pucit.edu.pk>
2021-05-25 13:28:58 +0500

    on create event added to app class

--- Notifications/app/src/main/java/com/example/notifications_practice/App.java
+++ Notifications/app/src/main/java/com/example/notifications_practice/App.java
@@ -6,5 +6,8 @@ public class App extends Application {
     public static final String CHANNEL_1_ID = "channel1";
     public static final String CHANNEL_2_ID = "channel2";
     public static final String CHANNEL_3_ID = "channel3";
-
+    public void onCreate() {
+        super.onCreate();
+        //createNotificationChannels();
+    }
 }
