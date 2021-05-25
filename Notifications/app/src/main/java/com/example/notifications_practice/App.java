34cf4b837f2e91a2d917962b9355b0a19b563b1b
farahbutt99 <mcsf19a021@pucit.edu.pk>
2021-05-25 13:28:13 +0500

    App class extended from application class

--- Notifications/app/src/main/java/com/example/notifications_practice/App.java
+++ Notifications/app/src/main/java/com/example/notifications_practice/App.java
@@ -1,4 +1,10 @@
 package com.example.notifications_practice;
 
-public class App {
+import android.app.Application;
+
+public class App extends Application {
+    public static final String CHANNEL_1_ID = "channel1";
+    public static final String CHANNEL_2_ID = "channel2";
+    public static final String CHANNEL_3_ID = "channel3";
+
 }
